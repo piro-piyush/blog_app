621a4c09221673cc2c560b41cce059d3add7927f
piyush <piyush72717272@gmail.com>
2025-01-23 13:31:54 +0530

    app_secrets

--- lib/core/secrets/app_secrets.dart
+++ lib/core/secrets/app_secrets.dart
@@ -1,5 +1,5 @@
 class AppSecrets {
-  static const supabaseUrl = "https://lrpgnystdhkkvxthculz.supabase.co";
+  static const supabaseUrl = "https://ufsnhchlcajpoifvkynn.supabase.co";
   static const supabaseAnonKey =
-      "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImxycGdueXN0ZGhra3Z4dGhjdWx6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzU1MzkwMjksImV4cCI6MjA1MTExNTAyOX0.1LcAgku8Z6qS1otPVQR7Ov6WsepZENpwtLFHhDWZTp4";
+      "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVmc25oY2hsY2FqcG9pZnZreW5uIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Mzc2MTUxNTcsImV4cCI6MjA1MzE5MTE1N30.NOJ6mFyHMoLB2Sh9YmO1ZM_WE3M-fIe_bxqkbPt8O5o";
 }
